b279a4107f269b8a226564999521a2f222acd3bc
Jose Aguilar <aguilarpro@gmail.com>
2019-04-09 19:09:40 -0400

    Initial Release.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace std;
+
+int main() {
+    std::string age;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "What is your Name? " << std::endl;
+    std::cin >> age;
+    std::cout << "Your age is : " << age << std::endl;
+    return 0;
+}
